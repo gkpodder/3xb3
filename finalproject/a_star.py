6bb47392f83c1e086a17e0afd793f1567d28ed2c
ajaygrewal <ajaygrewal2003@gmail.com>
2023-12-04 17:40:17 -0500

    part 3

--- finalproject/a_star.py
+++ finalproject/a_star.py
@@ -1,11 +1,30 @@
 from final_project_part1 import DirectedWeightedGraph
 import min_heap2
+import math
+import pandas as pd
 
+stationslist=pd.read_csv("london_stations.csv")
+connectionslist=pd.read_csv("london_connections.csv")
+
+
+graph_of_London=DirectedWeightedGraph()
+
+for i, r in stationslist.iterrows():
+    graph_of_London.add_node(r['id'])
+
+for i, r in connectionslist.iterrows():
+    graph_of_London.add_edge(r['station1'],r['station2'],r['time'])
+
+station_coordinates = {r['id']:(r['latitude'],r['longitude']) for i, r in stationslist.iterrows()}
 
 def heuristic(node, goal):
     # It should estimate the cost from the current node to the goal node
     # The heuristic function should be admissible (never overestimates the true cost)
     # use Euclidean distance as a heuristic function
+    #distance=sqrt((x2-x1)^2+(y2-y1)^2)
+    node_coords = station_coordinates[node]
+    goal_coords = station_coordinates[goal]
+    return math.sqrt((goal_coords[0] - node_coords[0])**2 + (goal_coords[1] - node_coords[1])**2)
     pass
 
 
@@ -41,3 +60,11 @@ def a_star(G, source, goal):
                 pred[neighbour] = current_node
 
     return dist
+
+
+start_id = 13  # Replace with actual source station ID
+goal_id = 279   # Replace with actual goal station ID
+generated_path = a_star(graph_of_London, start_id, goal_id)
+
+print(generated_path)
+
