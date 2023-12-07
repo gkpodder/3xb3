d148a7ece0e2810f789ced865f386cb8a07d99be
Gourob Podder <52678361+gkpodder@users.noreply.github.com>
2023-12-07 03:11:23 -0500

    fixed a*

--- finalproject/a_star.py
+++ finalproject/a_star.py
@@ -6,36 +6,40 @@ import timeit
 import matplotlib.pyplot as plt
 
 
-stationslist=pd.read_csv("london_stations.csv")
-connectionslist=pd.read_csv("london_connections.csv")
+stationslist = pd.read_csv("london_stations.csv")
+connectionslist = pd.read_csv("london_connections.csv")
 
 
-graph_of_London=DirectedWeightedGraph()
+graph_of_London = DirectedWeightedGraph()
 
 for i, r in stationslist.iterrows():
     graph_of_London.add_node(r['id'])
 
 for i, r in connectionslist.iterrows():
-    graph_of_London.add_edge(r['station1'],r['station2'],r['time'])
-    
+    graph_of_London.add_edge(r['station1'], r['station2'], r['time'])
 
-station_coordinates = {r['id']:(r['latitude'],r['longitude']) for i, r in stationslist.iterrows()}
+
+station_coordinates = {r['id']: (r['latitude'], r['longitude'])
+                       for i, r in stationslist.iterrows()}
+
 
 def heuristic(station1, station2, coordinates):
     coord1 = coordinates[station1]
     coord2 = coordinates[station2]
     return math.sqrt((coord2[0] - coord1[0])**2 + (coord2[1] - coord1[1])**2)
 
-def make_h(stations,station_coordinates):
+
+def make_h(stations, station_coordinates):
     precalculated_heuristics = {}
     for source in stations:
         for goal in stations:
             if source != goal:
-                precalculated_heuristics[(source, goal)] = heuristic(source, goal, station_coordinates)
+                precalculated_heuristics[(source, goal)] = heuristic(
+                    source, goal, station_coordinates)
     return precalculated_heuristics
 
 
-def a_star(G, source, goal,h):
+def a_star(G, source, goal, h):
     pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
     dist = {}  # Distance dictionary
     Q = min_heap2.MinHeap([])
@@ -45,29 +49,35 @@ def a_star(G, source, goal,h):
     for node in nodes:
         Q.insert(min_heap2.Element(node, float("inf")))
         dist[node] = float("inf")
-    Q.decrease_key(source, 0)
+    Q.decrease_key(source, 0 + h[(source, goal)])
 
     # Meat of the algorithm
     while not Q.is_empty():
         current_element = Q.extract_min()
         current_node = current_element.value
-        dist[current_node] = current_element.key
 
         if current_node == goal:
             break  # Exit early if the goal is reached
 
+        dist[current_node] = current_element.key - h[(current_node, goal)]
+
         for neighbour in G.adj[current_node]:
             tentative_g = dist[current_node] + G.w(current_node, neighbour)
-            tentative_f = tentative_g + h[(current_node, neighbour)]  # A* specific part
+            if neighbour == goal:
+                tentative_f = tentative_g
+            else:
+                tentative_f = tentative_g + \
+                    h[(neighbour, goal)]  # A* specific part
 
             if tentative_f < dist[neighbour]:
                 Q.decrease_key(neighbour, tentative_f)
                 dist[neighbour] = tentative_f
                 pred[neighbour] = current_node
 
-    return dist,pred
+    return dist, pred
 
-def dj_2(G, source,goal):
+
+def dj_2(G, source, goal):
     pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
     dist = {}  # Distance dictionary
     Q = min_heap2.MinHeap([])
@@ -87,9 +97,11 @@ def dj_2(G, source,goal):
         for neighbour in G.adj[current_node]:
             if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
                 Q.decrease_key(
-                    neighbour, dist[current_node] + G.w(current_node, neighbour)
+                    neighbour, dist[current_node] +
+                    G.w(current_node, neighbour)
                 )
-                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                dist[neighbour] = dist[current_node] + \
+                    G.w(current_node, neighbour)
                 pred[neighbour] = current_node
 
     return dist[goal]
@@ -125,58 +137,71 @@ def dj(G, source):
         for neighbour in G.adj[current_node]:
             if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
                 Q.decrease_key(
-                    neighbour, dist[current_node] + G.w(current_node, neighbour)
+                    neighbour, dist[current_node] +
+                    G.w(current_node, neighbour)
                 )
-                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                dist[neighbour] = dist[current_node] + \
+                    G.w(current_node, neighbour)
                 pred[neighbour] = current_node
 
     return dist
 
+
 stations = [r['id'] for i, r in stationslist.iterrows()]
-def timing_exp(stations,graph_of_London,h):
+
+
+def timing_exp(stations, graph_of_London, h):
     for source in stations:
         for goal in stations:
-                if source == goal:
-                    continue
-                a_star(graph_of_London, source, goal,h)
-            
-def dj_timing(graph,stations):
-    
+            if source == goal:
+                continue
+            a_star(graph_of_London, source, goal, h)
+
+
+def dj_timing(graph, stations):
+
     for source in stations:
         dj(graph, source)
 
+
 def experiment_every_pair():
-    h = make_h(stations,station_coordinates)
-    result_a_star=timeit.timeit(lambda: timing_exp(stations,graph_of_London,h),number=1)
-    result_dj = timeit.timeit(lambda: dj_timing(graph_of_London,stations),number=1)
+    h = make_h(stations, station_coordinates)
+    result_a_star = timeit.timeit(lambda: timing_exp(
+        stations, graph_of_London, h), number=1)
+    result_dj = timeit.timeit(lambda: dj_timing(
+        graph_of_London, stations), number=1)
     print(result_a_star)
     print(result_dj)
-    
+
+
 experiment_every_pair()
+
 
 def find_lines():
     lines_dictionary = {}
     connections = pd.read_csv("london_connections.csv")
     for line, setting in connections.groupby('line'):
-        lines_dictionary[line] = set(setting['station1']).union(set(setting['station2']))
+        lines_dictionary[line] = set(
+            setting['station1']).union(set(setting['station2']))
     return lines_dictionary
+
 
 def experiment_same_line():
     dijkstra_times = []
     astar_times = []
-    h = make_h(stations,station_coordinates)
-    lines = find_lines() 
+    h = make_h(stations, station_coordinates)
+    lines = find_lines()
     m_station_line = max(lines, key=lambda line_id: len(lines[line_id]))
     stations_sorted = sorted(lines[m_station_line])
-    
+
     for station_start in stations_sorted:
         time_dijkstra = 0
         time_a_star = 0
         for station_end in lines[m_station_line]:
             if station_start == station_end:
-                continue 
+                continue
             start_time = timeit.default_timer()
-            a_star(graph_of_London, station_start, station_end,h)
+            a_star(graph_of_London, station_start, station_end, h)
 
             time_a_star += timeit.default_timer() - start_time
 
@@ -194,8 +219,8 @@ def experiment_same_line():
     plt.legend()
     plt.show()
 
-experiment_same_line()
 
+experiment_same_line()
 
 
 def diff_lines():
@@ -205,7 +230,7 @@ def diff_lines():
     lines = find_lines()
     line1_stations = list(lines[1])
     line4_stations = list(lines[4])
-    h = make_h(stations,station_coordinates)
+    h = make_h(stations, station_coordinates)
 
     for s1 in line1_stations:
         dijkstra_time = 0
@@ -215,7 +240,7 @@ def diff_lines():
             if s1 == s2:
                 continue
             start = timeit.default_timer()
-            a_star(londonGraph, s1, s2,h)
+            a_star(londonGraph, s1, s2, h)
             a_star_time += timeit.default_timer() - start
             start = timeit.default_timer()
             dj_2(londonGraph, s1, s2)
@@ -231,7 +256,10 @@ def diff_lines():
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
 diff_lines()
+
 
 def experiment_multiple_transfers():
     dijkstra_times = []
@@ -239,7 +267,8 @@ def experiment_multiple_transfers():
     h = make_h(stations, station_coordinates)
     lines = find_lines()
 
-    transfer_stations_pairs = [(132, 268), (165, 216), (206, 217), (266, 268), (68, 283), (16, 93), (53, 160), (144, 278), (185, 279), (218, 227)] 
+    transfer_stations_pairs = [(132, 268), (165, 216), (206, 217), (266, 268), (
+        68, 283), (16, 93), (53, 160), (144, 278), (185, 279), (218, 227)]
     for (source, destination) in transfer_stations_pairs:
         time_dijkstra = 0
         time_a_star = 0
@@ -256,9 +285,16 @@ def experiment_multiple_transfers():
     plt.plot(dijkstra_times, label="Dijkstra's Algorithm")
     plt.xlabel("Pair Index")
     plt.ylabel("Total Time (seconds)")
-    plt.title("Performance Comparison of A* and Dijkstra for Stations Requiring Transfers")
+    plt.title(
+        "Performance Comparison of A* and Dijkstra for Stations Requiring Transfers")
     plt.legend()
     plt.show()
 
+
 experiment_multiple_transfers()
 
+"""
+Potential Problems:
+We used time as the weights but some people use the long and lat as the edge weights
+2. A* implementation might not be accurate
+"""
--- finalproject/refactored.py
+++ finalproject/refactored.py
@@ -100,7 +100,7 @@ class Bellman_Ford(SPAlgorithm):
         dist[source] = 0
 
         # Meat of the algorithm
-        for _ in range(G.number_of_nodes()):
+        for _ in range(G.get_num_of_nodes()):
             for node in nodes:
                 for neighbour in G.adj[node]:
                     if dist[neighbour] > dist[node] + G.w(node, neighbour):
@@ -113,43 +113,46 @@ class A_Star(SPAlgorithm):
     def calc_sp(self, G: Graph, source: int, dest: Optional[int] = None) -> float:
         try:
             heuristic = G.get_heuristic()
+            print("hello", heuristic)
         except AttributeError:
             raise ValueError("Heuristic not set")
-        return a_star(G, source, dest, heuristic)[0][dest]
+        return self.a_star(G, source, dest, heuristic)[0][dest]
 
+    def a_star(self, G, source, goal, h):
+        pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
+        dist = {}  # Distance dictionary
+        Q = min_heap2.MinHeap([])
+        nodes = list(G.adj.keys())
 
-def a_star(G, source, goal, h):
-    pred = {}  # Predecessor dictionary. Isn't returned, but here for your understanding
-    dist = {}  # Distance dictionary
-    Q = min_heap2.MinHeap([])
-    nodes = list(G.adj.keys())
+        # Initialize priority queue/heap and distances
+        for node in nodes:
+            Q.insert(min_heap2.Element(node, float("inf")))
+            dist[node] = float("inf")
+        Q.decrease_key(source, 0 + h[(source, goal)])
 
-    # Initialize priority queue/heap and distances
-    for node in nodes:
-        Q.insert(min_heap2.Element(node, float("inf")))
-        dist[node] = float("inf")
-    Q.decrease_key(source, 0)
+        # Meat of the algorithm
+        while not Q.is_empty():
+            current_element = Q.extract_min()
+            current_node = current_element.value
+            if current_node == goal:
+                break  # Exit early if the goal is reached
 
-    # Meat of the algorithm
-    while not Q.is_empty():
-        current_element = Q.extract_min()
-        current_node = current_element.value
-        dist[current_node] = current_element.key
+            dist[current_node] = current_element.key - h[(current_node, goal)]
 
-        if current_node == goal:
-            break  # Exit early if the goal is reached
+            for neighbour in G.adj[current_node]:
+                tentative_g = dist[current_node] + G.w(current_node, neighbour)
+                if neighbour == goal:
+                    tentative_f = tentative_g
+                else:
+                    tentative_f = tentative_g + \
+                        h[(neighbour, goal)]  # A* specific part
 
-        for neighbour in G.adj[current_node]:
-            tentative_g = dist[current_node] + G.w(current_node, neighbour)
-            tentative_f = tentative_g + \
-                h[(current_node, neighbour)]  # A* specific part
+                if tentative_f < dist[neighbour]:
+                    Q.decrease_key(neighbour, tentative_f)
+                    dist[neighbour] = tentative_f
+                    pred[neighbour] = current_node
 
-            if tentative_f < dist[neighbour]:
-                Q.decrease_key(neighbour, tentative_f)
-                dist[neighbour] = tentative_f
-                pred[neighbour] = current_node
-
-    return dist, pred
+        return dist, pred
 
 
 class ShortPathFinder:
@@ -173,5 +176,45 @@ class ShortPathFinder:
 g = WeightedGraph()
 g.add_node(1)
 g.add_node(2)
+g.add_node(3)
+g.add_node(4)
+g.add_node(5)
 g.add_edge(1, 2, 3.5)
-print(g.weights)
+g.add_edge(1, 3, 5)
+g.add_edge(2, 3, 1)
+g.add_edge(2, 4, 2)
+g.add_edge(3, 4, 1)
+g.add_edge(3, 5, 4)
+g.add_edge(4, 5, 1)
+
+sp = ShortPathFinder()
+sp.set_graph(g)
+print("Adjacency list: ", sp.graph.adj)
+print("Graph weights: ", sp.graph.weights)
+sp.set_algorithm(Bellman_Ford())
+print(f"Bellman Ford : {sp.calc_short_path(1, 5)}")
+sp.set_algorithm(Dijkstra())
+print(f"Djikstra : {sp.calc_short_path(1, 5)}")
+# abritray heuristic just to check if it works isn't really a good measure
+h = HeuristicGraph()
+h.add_node(1)
+h.add_node(2)
+h.add_node(3)
+h.add_node(4)
+h.add_node(5)
+h.add_edge(1, 2, 3.5)
+h.add_edge(1, 3, 5)
+h.add_edge(2, 3, 1)
+h.add_edge(2, 4, 2)
+h.add_edge(3, 4, 1)
+h.add_edge(3, 5, 4)
+h.add_edge(4, 5, 1)
+h.heuristic[(1, 5)] = 1
+h.heuristic[(2, 5)] = 2
+h.heuristic[(3, 5)] = 3
+h.heuristic[(4, 5)] = 4
+print(h.heuristic)
+sh = ShortPathFinder()
+sh.set_graph(h)
+sh.set_algorithm(A_Star())
+print(f"A* : {sh.calc_short_path(1, 5)}")
